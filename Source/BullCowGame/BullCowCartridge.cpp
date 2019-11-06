56c3fd64c1c8bb662bdada3c49f7bc4bbd30f0d5
lununglee <leo.lunung.lee@gmail.com>
2019-11-06 12:42:30 -0800

    U08 - Coding Basic Game Loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -6,13 +6,18 @@ void UBullCowCartridge::BeginPlay()
 {
 	Super::BeginPlay();
 	PrintLine(TEXT("You're playing the BullCowGame!"));
-	PrintLine(TEXT("Please type something into the terminal and press \"Enter\""));
+	PrintLine(TEXT("Please guess a 4 letter isogram (words without repeating characters)"));
+	PrintLine(TEXT("Please press \"Enter\" to continue"));
 };
 
 //When the player hits enter
 void UBullCowCartridge::OnInput(const FString& Input)
 {
 	ClearScreen();
-	FString HiddenWord = TEXT("star");
-	PrintLine(Input);
+	FString HiddenWord = TEXT("star");	// move outside function
+	if (Input == HiddenWord)
+		PrintLine(TEXT("You won!"));
+	else
+		PrintLine(TEXT("You lost..."));
+
 };
