9f1a4ac46e5558696521bf796fcab75ec35ffc07
lununglee <leo.lunung.lee@gmail.com>
2019-11-06 19:58:03 -0800

    U10 - Pseudo Code

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,19 +5,44 @@
 void UBullCowCartridge::BeginPlay()
 {
 	Super::BeginPlay();
-	HiddenWord = TEXT("star");
 
+	// Welcoming the player
 	PrintLine(TEXT("You're playing the BullCowGame!"));
 	PrintLine(TEXT("Please guess a 4 letter isogram (words without repeating characters)"));
 	PrintLine(TEXT("Please press \"Enter\" to continue"));
+	
+	// Set the HiddenWord
+	HiddenWord = TEXT("star");
+
+	// Set Lives
+
+	// Prompt player for PlayerGuess
 };
 
-//When the player hits enter
+// When player hits "Enter"
 void UBullCowCartridge::OnInput(const FString& Input)
 {
 	ClearScreen();
+
+	// Checking PlayerGuess
 	if (Input == HiddenWord)
+	{
 		PrintLine(TEXT("You won!"));
+	}
 	else
+	{
 		PrintLine(TEXT("You lost..."));
+	}
+
+	// Check if PlayerGuess is Isogram
+		// Prompt to guess again
+	// Check if PlayerGuess has the right number of characters
+		// Prompt to guess again
+	// Check if Life > 0
+
+	// Remove Life
+
+	// Does user want to play again?
+		// If yes, repeat (set next word)
+		// If no, print game failed and show HiddenWord
 };
